92e726014149fe1473a225d25b477b8195e67eb5
Nguyễn Thành Luân <75401588+takarahunter@users.noreply.github.com>
2022-08-29 11:27:02 +0700

    create demo active MQ project

---
+++ ActiveMQ_Demo/src/main/java/lab01/iuh/ActiveMQ_Demo/ActiveMqDemoApplication.java
@@ -0,0 +1,13 @@
+package lab01.iuh.ActiveMQ_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMqDemoApplication.class, args);
+	}
+
+}
---
+++ ActiveMQ_Demo/src/test/java/lab01/iuh/ActiveMQ_Demo/ActiveMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lab01.iuh.ActiveMQ_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
